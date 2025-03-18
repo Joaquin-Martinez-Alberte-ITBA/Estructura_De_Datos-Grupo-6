8f4f2f02de727ab83e4603203185d7513eca1b0a
Joaquin <Jmartinezalberte@itba.edu.ar>
2025-03-18 19:17:39 -0300

    hola

--- Guia_2.py
+++ Guia_2.py
@@ -1,4 +1,11 @@
 ##Ejercicio 1
-
-
-'hola'
+'''
+str = "cadena de texto123"
+letras = sum(char.isalpha() for char in str)
+numericos = sum(char.isnumeric() for char in str)
+especiales = len(str) - letras - numericos
+print(letras)
+print(numericos)
+print(especiales)
+'''
+##Ejercicio 2 
