f285da15cb30d4f86f17d5eaf982ae92f52a780e
Juangrispoo <juanvgrispo@gmail.com>
2025-04-15 18:23:43 -0300

    carga datos juan

--- Guia_2.py
+++ Guia_2.py
@@ -1,5 +1,4 @@
 ##Ejercicio 1
-'''
 str = "cadena de texto123"
 letras = sum(char.isalpha() for char in str)
 numericos = sum(char.isnumeric() for char in str)
@@ -7,27 +6,27 @@ especiales = len(str) - letras - numericos
 print(letras)
 print(numericos)
 print(especiales)
-'''
+
 ##Ejercicio 2 
-'''
+
 str = "contar caracater especifico"
 char = 'a'
 print(str.count(char))'
-'''
+
 ##Ejercicio 3
-'''
+
 str = "Cambiar a mayus"
 print(str.swapcase())'
-'''
+
 ##Ejercicio 5
-'''
+
 str = "Estructura de datos alfa"
 lista = str.split()
 print(max(lista,key=len))
 print(min(lista,key=len))'
-'''
+
 #Ejercicio 6
-'''
+
 str = "Estructura de datos alfa"
 lista = list(set(str))
 for a in lista:
@@ -36,15 +35,15 @@ for a in lista:
         if char == a:
             num +=1
     print(a,':',num)
-'''
+
 #Ejercicio 7
-'''
+
 Palabras=["La","materia","Estructuras","de","Datos","es","genial"]
 primeraPalabra = 'La'
 segundaPalabra = 'es'
 diff = Palabras.index(segundaPalabra) - Palabras.index(primeraPalabra)
 print(diff)
-'''
+
 #Ejercicio 8
 
 
